57cd023df5d4287b434d3e31d2a8124e3a8cfec7
Unknown <jules.v.paget@gmail.com>
2017-09-26 10:08:15 +0200

    Create all classes

--- model.js
+++ model.js
@@ -1,3 +1,22 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
+function Form (couleur,epaisseur){
+        this.couleur = couleur;
+        this.epaisseur = epaisseur;
+};
+
+function Drawing (){
+        var listform = [];
+}
+//Un rectangle possède des coordonnées de son point haut-gauche, une largeur et une hauteur.
+function Rectangle (x,y,largeur,hauteur){
+    Form.call(x,y,largeur,hauteur,this);
+};  
+Rectangle.prototype = new Form();
+
+// Une ligne possède les coordonnées de ses deux points.
+function Line (x,y){
+    Line.call(x,y,this) ;
+};
+Line.prototype = new Form() ;
