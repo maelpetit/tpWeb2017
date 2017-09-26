4e7b86c501015fcc39a5840d2f3ef1eed498eaba
Unknown <mael.petit@wanadoo.fr>
2017-09-26 10:21:27 +0200

    yo

--- model.js
+++ model.js
@@ -1,23 +1,31 @@
 
 // Implémenter ici les 4 classes du modèle.
 // N'oubliez pas l'héritage !
-function Form (couleur,epaisseur){
-        this.couleur = couleur;
-        this.epaisseur = epaisseur;
+function Form(color_, width_) {
+    var color = color_;
+    var width = width_;
 };
 
-function Drawing (){
-        var forms = new Array();
+function Drawing() {
+    var forms = new Array();
 }
 
 //Un rectangle possède des coordonnées de son point haut-gauche, une largeur et une hauteur.
-function Rectangle (x,y,largeur,hauteur){
-    Form.call(x,y,largeur,hauteur,this);
-};  
+function Rectangle(X0, Y0, X1, Y1, color, width) {
+    Form.call(this, color, width);
+    var x0 = X0;
+    var y0 = Y0;
+    var x1 = X1;
+    var y1 = Y1;
+};
 Rectangle.prototype = new Form();
 
 // Une ligne possède les coordonnées de ses deux points.
-function Line (x,y){
-    Line.call(x,y,this) ;
+function Line(X0, Y0, X1, Y1, color, width) {
+    Form.call(this, color, width);
+    var x0 = X0;
+    var y0 = Y0;
+    var x1 = X1;
+    var y1 = Y1;
 };
-Line.prototype = new Form() ;
+Line.prototype = new Form();
